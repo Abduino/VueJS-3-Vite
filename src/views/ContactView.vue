0f54293eb494510007e37d2ce938897e27875515
Abduino <abdrehmanredi0@gmail.com>
2022-11-04 15:13:18 -0300

    contact page updated

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,15 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+const txt1 = ref("txt1")
+const txt2 = ref("txt2")
+function getInput(){
+  txt1.value =  "i got you"
+  txt2.value = "i got u too"
+
+}
+
+</script>
+
 <template>
     <div class="contact">
       <h1>This is contact page</h1>
     </div>
     <div class="form">
-      
         <input type="text" name="txtFname"/><br/>
         <input type="text" name="txtLname"/><br/>
         <button name="btnSubmit">Submit</button> 
-
-    
-
+    </div>
+    <div>
+      <h3>inputs .......</h3>
+      <p>{{txt1}}</p>
+      <p>{{txt2}}</p>
+      <button @click="getInput">Get</button>
     </div>
   </template>
   
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,27 @@
+<script>
+import  {ref}  from 'vue';
+console.log("main func")
+// const dat = ref(0)
+const isPlaying = ref(false)
+console.log("declered")
+
+function timerStart(){
+  console.log("inner func")
+  // dat.value = 200
+  isPlaying.value = true
+  // console.log(dat)
+  console.log(isPlaying)
+}
+</script>
 <template>
     <div class="profile">
       <h1>This is profile page</h1>
+      <button @click="timerStart">play</button>
+      <h3>This is my real data</h3>
+      <h2>{{isPlaying}}</h2>
     </div>
   </template>
+
   
   <style>
   /* @media (min-width: 1024px) {

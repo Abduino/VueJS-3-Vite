3859ab60d4c4e41262d3d5eea5627563040c1c2d
Abduino <abdrehmanredi0@gmail.com>
2022-11-14 11:08:20 -0400

    variable diclaration finished

--- src/views/GuessView.vue
+++ src/views/GuessView.vue
@@ -33,5 +33,17 @@
 </template>
 
 <script setup>
+// variable diclaration
+let randomNumber = Math.floor(Math.random()*100) + 1;
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit= document.querySelector('.guessSubmit');
+const guessField= document.querySelector('.guessField');
+
+let guessCount =1;
+let resetButton;
+
 
 </script>
